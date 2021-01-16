9f2dd47b37f6b52ace8d7bd8c4b697c238c62b44
Walter <U201620367@upc.edu.pe>
2021-01-16 14:54:26 -0500

    agregar stepdefinition

---
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -0,0 +1,25 @@
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitions {
+    @Given("(.*) has an active account")
+    public void has_an_active_account(String a) {
+
+    }
+
+    @When("he sends their valid credentials")
+    public void he_sends_their_valid_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("he should have access to manage his account")
+    public void he_should_have_access_to_manage_his_account() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
